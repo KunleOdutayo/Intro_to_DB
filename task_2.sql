ef57caa822a65551cd786752993a0424bee4261b
Kunle Odutayo <kunlay.odutayo@gmail.com>
2025-08-17 20:25:17 +0100

    create task_2.sql file

--- task_2.sql
+++ task_2.sql
@@ -29,7 +29,7 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE Order_details (
+CREATE TABLE Order_Details (
     orderdetail INT PRIMARY KEY,
     order_id INT,
     book_id,
