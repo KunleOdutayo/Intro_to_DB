881d701a2d9885ec4f4a609cec40c67fab71e251
Kunle Odutayo <kunlay.odutayo@gmail.com>
2025-08-17 20:50:35 +0100

    create task_5.sql file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -19,7 +19,8 @@ CREATE TABLE Customers (
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
     address TEXT
-);CREATE TABLE Orders (
+);
+CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
