df148f070d3a2c26e0e82eb55a45a0f3a48d0afa
Kunle Odutayo <kunlay.odutayo@gmail.com>
2025-08-17 19:40:51 +0100

    Intro to sql beginning

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS alx_books_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-USE alx_books_store;
+USE alx_book_store;
 
 CREATE TABLE Authors (
     aurthor_id INT PRIMARY KEY,
