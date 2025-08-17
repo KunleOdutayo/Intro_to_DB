33eff2db0891a3f78a5966ce079ebcdf54d1c454
Kunle Odutayo <kunlay.odutayo@gmail.com>
2025-08-17 19:38:02 +0100

    Intro to sql beginning

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -21,4 +21,10 @@ CREATE TABLE Customers (
     order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
-CREATE TABLE 
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
